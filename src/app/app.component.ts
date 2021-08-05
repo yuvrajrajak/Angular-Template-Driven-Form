85379d3dab49332fc81367c6bebdea46d5d3dd3a
Yuvraj <yuvrajrajak538@gmail.com>
2021-08-05 17:34:05 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { EnrollmentService } from './enrollment.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'tdf';
+  topics = ['Angular', 'Vue', 'React'];
+  topicHasError = true;
+  submitted = false;
+  errorMsg = '';
+  userModel = new User('', 'rob@test.com', 45662133, '', 'morning', true);
+
+  constructor(private enrollmentService: EnrollmentService) {}
+
+  validateTopic(value: any) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.enrollmentService.enroll(this.userModel).subscribe(
+      (data) => console.log('Success!', data),
+      (error) => this.errorMsg = error.statusText
+    );
+    console.log(this.userModel);
+  }
 }
---
+++ src/app/enrollment.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { User } from './user';
+import { catchError } from 'rxjs/operators'
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnrollmentService {
+  
+  apiUrl = 'http://localhost:3000/enroll';
+  constructor(private http: HttpClient) { }
+  enroll(user: User){
+  return this.http.post<any>(this.apiUrl,user).pipe(catchError(this.errorHandler))
+  }
+
+  errorHandler(error: HttpErrorResponse){
+  return throwError(error)
+  }
+}
